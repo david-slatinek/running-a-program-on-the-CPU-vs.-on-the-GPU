952e9e7087e0f21f8daa6b02bb718313fa7c0a5e
David Slatinek <david.slatinek5@gmail.com>
2021-06-23 22:01:08 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 docker image build -t cpu_gpu_main .
 
 docker container run -d --name cpu_gpu_container_main cpu_gpu_main
@@ -6,8 +6,8 @@ docker container cp cpu_gpu_container_main:app/cpu.json cpu.json
 docker container cp cpu_gpu_container_main:app/gpu.json gpu.json
 docker container rm cpu_gpu_container_main
 
-#docker container run -it --name cpu_gpu_container_main cpu_gpu_main bash
-#python3 main.py
+docker container run -it --name cpu_gpu_container_main cpu_gpu_main bash
+python3 main.py
 
 
 
@@ -17,5 +17,5 @@ docker container run -d --name cpu_gpu_container_plot cpu_gpu_plot
 docker container cp cpu_gpu_container_plot:app/image.jpg image.jpg
 docker container rm cpu_gpu_container_plot
 
-#docker container run -it --name cpu_gpu_container_plot cpu_gpu_plot bash
-#python3 plotting.py
+docker container run -it --name cpu_gpu_container_plot cpu_gpu_plot bash
+python3 plotting.py
